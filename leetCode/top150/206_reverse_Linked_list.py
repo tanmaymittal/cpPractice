fb3cf45ebc7509019a0c9310480aae6ce0d783c4
tmittal1 <tmittal1@ucsc.edu>
2021-06-12 12:26:54 +0530

    reverse ll done

--- leetCode/top150/206_reverse_Linked_list.py
+++ leetCode/top150/206_reverse_Linked_list.py
@@ -24,3 +24,25 @@ Constraints:
 The number of nodes in the list is the range [0, 5000].
 -5000 <= Node.val <= 5000
 """
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """
+        so store head in a temp 
+        """ 
+        p = None 
+        while(head):
+            curr = head 
+            head = head.next 
+            curr.next = p
+            p = curr
+        return p 
+            
+"""
+Could not solve myself. Watched a video and did it again using paper and pen 12 hrs later.
+"""
